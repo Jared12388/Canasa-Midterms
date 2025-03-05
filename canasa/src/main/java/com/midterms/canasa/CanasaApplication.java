464ba9d5657dca99d0dffb3e1f40e7d0cbb63869
Jared12388 <canasajared666@gmail.com>
2025-03-05 23:54:19 +0800

    NEW COMMIT

---
+++ canasa/src/main/java/com/midterms/canasa/CanasaApplication.java
@@ -0,0 +1,13 @@
+package com.midterms.canasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanasaApplication.class, args);
+	}
+
+}
---
+++ canasa/src/test/java/com/midterms/canasa/CanasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midterms.canasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
